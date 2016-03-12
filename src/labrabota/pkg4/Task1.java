87c2d3389fc8f751d0c75f9b9f00f35475d375bc
unknown <bai1541b@k3-426-10.edu.ugrasu>
2016-03-12 12:28:24 +0500

    В 1-м задании изменена проверка ввода.

--- src/labrabota/pkg4/Task1.java
+++ src/labrabota/pkg4/Task1.java
@@ -1,6 +1,6 @@
 /*
  * Эта прог-а генерирует числа от а до б стат. методом и заполняет ими массив из 20 эл-ов.
-    Автор Бредихин А.И.
+ Автор Бредихин А.И.
  */
 
 import java.io.BufferedReader;
@@ -10,20 +10,18 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class Task1 {
-    
+
     public static int gen(int a, int b) {
-        int aa=(int)(Math.random()*(b-a))+1;
-        aa=aa+a;
+        int aa = (int) (Math.random() * (b - a)) + 1;
+        aa = aa + a;
         return aa;
     }
-    
+
     public static void main(String[] args) {
         int aa[];
-        aa=new int [20];
-        int i,a,b;
-        
-        String abcd="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMйцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ_{}[];:'/";
-        
+        aa = new int[20];
+        int i, a = 0, b = 0;
+
         InputStream inputStream = System.in;
         Reader inputStreamReader = new InputStreamReader(inputStream);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
@@ -31,13 +29,14 @@ public class Task1 {
         String s1 = null;
 
         System.out.print("Введите a: ");
-        
+
         try {
             s1 = bufferedReader.readLine(); //читаем строку с клавиатуры
-        } catch (IOException ex) {
+            a = Integer.parseInt(s1);
+        } catch (Exception ex) {
             System.out.println("\nОшибка считывания.");
+            return;
         }
-
 
         String s2 = null;
 
@@ -45,26 +44,21 @@ public class Task1 {
 
         try {
             s2 = bufferedReader.readLine(); //читаем строку с клавиатуры
-        } catch (IOException ex) {
+            b = Integer.parseInt(s2);
+        } catch (Exception ex) {
             System.out.println("\nОшибка считывания.");
+            return;
         }
-        
-        for (int j=0; j<abcd.length(); j++) {
-                if ((s1.contains(""+abcd.charAt(j))) || (s2.contains(""+abcd.charAt(j)))) {
-                System.out.print("Неправильные числа");
-                return;
-            }
+
+        if (a > b) {
+            System.out.print("Число a не должно быть больше b!");
+            return;
         }
-        
-        a=Integer.parseInt(s1);
-        b=Integer.parseInt(s2);
-        
-        if (a>b) {System.out.print("Число a не должно быть больше b!"); return;}
-        
-        for (i=0; i<20; i++)  {
-            aa[i]=gen(a,b);
-            System.out.print(aa[i]+" ");
+
+        for (i = 0; i < 20; i++) {
+            aa[i] = gen(a, b);
+            System.out.print(aa[i] + " ");
         }
-        
+
     }
 }
